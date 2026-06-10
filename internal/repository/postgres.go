b11c145d6b5d0466b983503b14a321d69d12bcce
uiNoiSE <uiNoiSE@gmail.com>
2026-06-10 22:13:51 +0300

    refactor(repository):
    introduce pgxQuerier interface and WithTx/BeginTx to repository.

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -5,10 +5,19 @@ import (
 	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type pgxQuerier interface {
+	Exec(ctx context.Context, sql string, args ...any) (pgconn.CommandTag, error)
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+}
+
 type PostgresRepository struct {
+	db   pgxQuerier
 	pool *pgxpool.Pool
 }
 
@@ -29,6 +38,7 @@ func NewPostgresPool(connStr string) (*pgxpool.Pool, error) {
 
 func NewPostgresRepository(pool *pgxpool.Pool) *PostgresRepository {
 	return &PostgresRepository{
+		db:   pool,
 		pool: pool,
 	}
 }
@@ -38,7 +48,7 @@ func (r *PostgresRepository) CreateWallet(ctx context.Context) (uuid.UUID, error
 
 	query := `INSERT INTO wallets (id, balance) VALUES ($1, $2)`
 
-	_, err := r.pool.Exec(ctx, query, newID, 0.00)
+	_, err := r.db.Exec(ctx, query, newID, 0.00)
 	if err != nil {
 		return uuid.Nil, fmt.Errorf("Ошибка при вставке кошелька в БД: %w", err)
 	}
@@ -51,7 +61,7 @@ func (r *PostgresRepository) GetWalletBalance(ctx context.Context, id uuid.UUID)
 
 	query := `SELECT balance FROM wallets WHERE id = $1`
 
-	err := r.pool.QueryRow(ctx, query, id).Scan(&balance)
+	err := r.db.QueryRow(ctx, query, id).Scan(&balance)
 	if err != nil {
 		return 0.0, fmt.Errorf("Ошибка при получении баланса кошелька: %w", err)
 	}
@@ -62,7 +72,7 @@ func (r *PostgresRepository) GetWalletBalance(ctx context.Context, id uuid.UUID)
 func (r *PostgresRepository) UpdateWalletBalance(ctx context.Context, id uuid.UUID, newBalance float64) error {
 	query := `UPDATE wallets SET balance = $1 WHERE id = $2`
 
-	result, err := r.pool.Exec(ctx, query, newBalance, id)
+	result, err := r.db.Exec(ctx, query, newBalance, id)
 	if err != nil {
 		return fmt.Errorf("Ошибка при обновлении баланса в БД: %w", err)
 	}
@@ -72,4 +82,18 @@ func (r *PostgresRepository) UpdateWalletBalance(ctx context.Context, id uuid.UU
 	}
 
 	return nil
+}
+
+func (r *PostgresRepository) WithTx(tx pgxQuerier) *PostgresRepository {
+	return &PostgresRepository{
+		db: tx,
+	}
+}
+
+func (r *PostgresRepository) BeginTx(ctx context.Context) (pgx.Tx, error) {
+	tx, err := r.pool.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("Не удалось открыть транзакцию: %w", err)
+	}
+	return tx, nil
 }
